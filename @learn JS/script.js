2d8cc0a1e37de85c6f43bed37a70eb165ce48172
tanishqkrk <tanishqkrk@gmail.com>
2022-02-27 14:27:03 +0530

    Renamed

--- @learn JS/script.js
+++
@@ -1,3 +0,0 @@
-for (let a = 1; a < 11; a++) {
-    console.log(a);
-}
---
+++ Project 19 Item Lister/script.js
@@ -0,0 +1,6 @@
+// let button = document.getElementsByClassName('lister-items-item');
+// for(let i = 0; i<button.length; i++){
+//     button[i].style.background = 'red';
+//     console.log(button[i]);
+// }
+
