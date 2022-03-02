19984132edf35db417e7c157b9f624be61a2ed1d
tanishqkrk <tanishqkrk@gmail.com>
2022-03-02 22:48:47 +0530

    JS

--- Project 19 Item Lister/script.js
+++ Project 19 Item Lister/script.js
@@ -4,3 +4,32 @@
 //     console.log(button[i]);
 // }
 
+// let newDiv = document.createElement('div');
+// newDiv.className = 'Ssup';
+// newDiv.id = 'Ssup';
+// newDiv.innerText = "Ssup";
+// console.log(newDiv);
+// let lister = document.querySelector('.lister-items-items');
+// console.log(lister);
+// let listerItems = document.querySelector('.lister-items-item');
+// console.log(listerItems);
+
+
+// lister.insertBefore(newDiv, listerItems)
+
+// newDiv.style.fontSize = '50px'
+// console.log(document.querySelector('.lister-items'));
+
+function myClick(){
+    // console.log('Bruh');
+    newItem = document.createElement('li');
+    newItem.innerText = 'NewElement!';
+    newItem.className = 'lister-items-item';
+    // console.log(newItem);
+
+    listContainer = document.querySelector('.lister-items-items');
+    listContainerElement = document.querySelector('.lister-items-item');
+    // console.log(listContainerElement);
+
+    listContainer.insertBefore(newItem, listContainerElement);
+}
