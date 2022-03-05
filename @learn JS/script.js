bdf4e4b8d89690c0bd195388309776ea7752d70a
tanishqkrk <tanishqkrk@gmail.com>
2022-03-05 22:53:25 +0530

    JS

---
+++ @learn JS/script.js
@@ -0,0 +1,106 @@
+// function create(){
+    // alert("Tanishq");
+    // console.log(create);
+//     let newElement = document.createElement('div');
+//     newElement.className = "element";
+//     newElement.innerText = "Hello!";
+//     let box = document.querySelector('.contentbase');
+//     let body = document.querySelector(".box");
+//     body.insertBefore(newElement, box);
+//   }
+
+//   function change(){
+//       newElementStyle = document.querySelectorAll(".element");
+//       for(let i = 0; i < newElementStyle.length; i++){
+//         newElementStyle[i].style.color = 'red';
+//       }
+
+// }
+
+// let main = document.body;
+
+// function red(){
+//   main.style.backgroundColor = 'red';
+// }
+// function blue(){
+//   main.style.backgroundColor = 'blue';
+// }
+// function green(){
+//   main.style.backgroundColor = 'green';
+// }
+// function yellow(){
+//   main.style.backgroundColor = 'yellow';
+// }
+// function grey(){
+//   main.style.backgroundColor = 'grey';
+// }
+// function black(){
+//   main.style.backgroundColor = 'black';
+// }
+// function white(){
+//   main.style.backgroundColor = 'white';
+// }
+
+// let main = document.body;
+// let buttonRed = document.querySelector(".buttons-red");
+// buttonRed.addEventListener('click',click)
+// function click(a){
+//   console.log(a.target.style);
+// }
+
+// let body = document.body;
+// let data = document.querySelector('.data');
+// // console.log(body);
+// data.addEventListener('mousemove',  position);
+// function position(move){
+//   body.style.background = `rgb(${move.offsetX}, ${move.offsetY}, ${Math.random(255)})`;
+// }
+
+// Item Lister JS Comments
+// let button = document.getElementsByClassName('lister-items-item');
+// for(let i = 0; i<button.length; i++){
+//     button[i].style.background = 'red';
+//     console.log(button[i]);
+// }
+
+// let newDiv = document.createElement('div');
+// newDiv.className = 'Ssup';
+// newDiv.id = 'Ssup';
+// newDiv.innerText = "Ssup";
+// console.log(newDiv);
+// let lister = document.querySelector('.lister-items-items');
+// console.log(lister);
+// let listerItems = document.querySelector('.lister-items-item');
+// console.log(listerItems);
+
+
+// lister.insertBefore(newDiv, listerItems)
+
+// newDiv.style.fontSize = '50px'
+// console.log(document.querySelector('.lister-items'));
+
+// let input = document.querySelector('#lister-input-input');
+// console.log(input);
+
+// input.addEventListener('cut', keyboard);
+// input.addEventListener('input', keyboard);
+// function keyboard(type){
+    // var newElement = document.createElement('li');
+    // newElement.className = 'lister-items-items';
+    // newElement.innerHTML = type.target.value;
+    // console.log( type.type);
+    // console.log(newElement);
+    // newElement.className = 'lister-items-item';
+    // newElement.textContent = type.type;
+    // console.log(newElement.target.value);
+    // document.querySelector('.lister-items-items').innerHTML = `${type.target.value}`;
+    // console.log(newElement);
+    // console.log(type.target.value);
+// }
+
+// let list = document.querySelector('.lister-items');
+// let button = document.querySelector('#lister-input-button');
+// button.addEventListener('click', addElement);
+// function addElement(){
+//     list.insertBefore(newElement, list);
+// }
--- Project 19 Item Lister/script.js
+++ Project 19 Item Lister/script.js
@@ -1,35 +1,9 @@
-// let button = document.getElementsByClassName('lister-items-item');
-// for(let i = 0; i<button.length; i++){
-//     button[i].style.background = 'red';
-//     console.log(button[i]);
-// }
+let lister = document.querySelector('.lister');
+let button = document.querySelector('.lister-input-button');
+// console.log(button);
+button.addEventListener('submit', buttonSubmit);
 
-// let newDiv = document.createElement('div');
-// newDiv.className = 'Ssup';
-// newDiv.id = 'Ssup';
-// newDiv.innerText = "Ssup";
-// console.log(newDiv);
-// let lister = document.querySelector('.lister-items-items');
-// console.log(lister);
-// let listerItems = document.querySelector('.lister-items-item');
-// console.log(listerItems);
-
-
-// lister.insertBefore(newDiv, listerItems)
-
-// newDiv.style.fontSize = '50px'
-// console.log(document.querySelector('.lister-items'));
-
-function myClick(){
-    // console.log('Bruh');
-    newItem = document.createElement('li');
-    newItem.innerText = 'NewElement!';
-    newItem.className = 'lister-items-item';
-    // console.log(newItem);
-
-    listContainer = document.querySelector('.lister-items-items');
-    listContainerElement = document.querySelector('.lister-items-item');
-    // console.log(listContainerElement);
-
-    listContainer.insertBefore(newItem, listContainerElement);
+function buttonSubmit(e){
+    e.preventDefault();
+    console.log('1');
 }
