bf0d2c349aee973e1b1ec8746135c2985c15ed4c
tanishqkrk <tanishqkrk@gmail.com>
2022-03-08 01:13:15 +0530

    note taking app

--- Project 19 Item Lister/script.js
+++ Project 19 Item Lister/script.js
@@ -1,9 +1,35 @@
-let lister = document.querySelector('.lister');
-let button = document.querySelector('.lister-input-button');
-// console.log(button);
-button.addEventListener('submit', buttonSubmit);
+let form = document.querySelector('.lister');
+let submit = document.querySelector('.lister-submit');
+let list = document.querySelector('.lister-lister');
 
-function buttonSubmit(e){
+form.addEventListener('submit', addItem);
+
+
+function addItem(e){
     e.preventDefault();
-    console.log('1');
+    // console.log(1);
+    let input = document.querySelector('.lister-input').value;
+    // console.log(input);
+    newItem = document.createElement('li');
+    let deleteBtn = document.createElement('button');
+    deleteBtn.setAttribute('type','button')
+    deleteBtn.className = 'lister-delete'
+    deleteBtn.innerText = 'X'
+    newItem.className = 'lister-item';
+    newItem.innerHTML = input;
+    newItem.appendChild(deleteBtn)
+    // console.log(newItem);
+    list.appendChild(newItem);
+    deleteBtn.addEventListener('click', removeItem);
+    form.reset()
+}
+
+let deleteBtn = document.querySelector('.lister-delete');
+function removeItem(e){
+    if(confirm('Are you sure?')){
+
+        e.target.parentNode.remove();
+    }
+    // console.log('1');
+    // e.parentElement.style.display = 'none';
 }
