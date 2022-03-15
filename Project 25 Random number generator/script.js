6264df451c4e852fd0487227c623bfa10a38092b
tanishqkrk <tanishqkrk@gmail.com>
2022-03-16 00:14:14 +0530

    new

--- Project 25 Random number generator/script.js
+++ Project 25 Random number generator/script.js
@@ -8,18 +8,26 @@ submit.addEventListener('click', random);
 
 function random(e){
     e.preventDefault();
-    if(check.checked == true){
-        let randomNumber = (Math.random()*parseFloat( to.value )+parseFloat( from.value));
-        // console.log(randomDecNumber);
-        let randomD = randomNumber.toFixed(2);
-        number.innerHTML = randomD;
+
+    if(from == null && to == null){
+        alert('Enter valid value');
     }
-    else if(check.checked == false){
-        let randomNumber = Math.floor(Math.random()*(to.value) + from.value+1);
-        // number.innerHTML = randomNumber;
-        // console.log(randomNumber);
-        number.innerHTML = randomNumber;
+    else{
+        if(check.checked == true){
+            let randomNumber = Math.random()*(parseInt(to.value)-parseInt(from.value))+parseInt(from.value);
+            // console.log(randomDecNumber);
+            let randomD = randomNumber.toFixed(2);
+            number.innerHTML = randomD;
+        }
+        else if(check.checked == false){
+            let randomNumber = Math.floor(Math.random()*(parseInt(to.value)-parseInt(from.value))+parseInt(from.value));
+            // number.innerHTML = randomNumber;
+            // console.log(randomNumber);
+            number.innerHTML = randomNumber;
+        }
     }
+
+    
 }
 
 check.addEventListener('click', move);
