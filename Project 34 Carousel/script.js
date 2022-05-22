642008b807c31b88294b85881f4d87d34fc741b8
tanishqkrk <tanishqkrk@gmail.com>
2022-05-22 23:15:36 +0530

    new

--- Project 34 Carousel/script.js
+++ Project 34 Carousel/script.js
@@ -1,21 +1,16 @@
- // console.log(images);
- let images = [];
- img = document.querySelectorAll('img');
- for (let i = 0; i < img.length; i++) {
-     images.push(img[i]);
- }
-//  console.log(images);
+document.querySelector('.next').addEventListener('click', next)
+document.querySelector('.previous').addEventListener('click', back)
 
- document.querySelector('.next').addEventListener('click', e => {
-     for(let i = 0; i<images.length; i++){
-         for(let j = 0; j<i.length; j++){
-             if(images[i].classList.contains('on')){
-                 images[i].classList.remove('on');
-                 images[i+1].classList.add('on');
-             }
-             break
-            }
-    }
- })
+// document.querySelector('.next').click()
 
-//  document.querySelector('.next').click()
+function next(e) {
+    // let state = 300;
+    document.querySelector('.container').scrollBy(170, 0);
+    // state += 20
+    // break;
+}
+
+function back(e) {
+    document.querySelector('.container').scrollBy(-170, 0);
+
+}
