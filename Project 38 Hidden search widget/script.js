04b0f6fd183cef1ece7d401c7115d9893a8410dd
tanishqkrk <tanishqkrk@gmail.com>
2022-06-16 14:16:20 +0530

    modified search

--- Project 38 Hidden search widget/script.js
+++ Project 38 Hidden search widget/script.js
@@ -4,9 +4,14 @@ icon.addEventListener('click', e => {
     input.classList.toggle('inputOn');
     icon.classList.toggle('labelOn');
     if (input.classList.contains('inputOn')) {
-        input.placeholder = 'Search'
+        input.placeholder = 'Search';
+        input.removeAttribute('disabled', "")
+        // input.style.color = 'black'
+
     } else {
         input.placeholder = '';
         input.value = '';
+        input.setAttribute('disabled', "")
+        // input.style.color = 'transparent'
     }
 })
